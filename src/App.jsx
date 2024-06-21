60d9aee3417d22ac72dba829db9196554abcad67
Steve <zjplove3618739792@gmail.com>
2024-06-21 11:13:20 -0400

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+// src/App.jsx
+
+const App = () => {
+  return <h1>Hello world!</h1>;
+};
+
+export default App;
